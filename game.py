5c483502136b1c17b99dfd447f5f97bc043a8553
zx12r4@yahoo.fr <zx12r4@yahoo.fr>
2018-11-27 15:52:10 +0100

    Game development with TDD

---
+++ game.py
@@ -0,0 +1,25 @@
+# game.py
+import random
+import string
+
+
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy()
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
+
+test = Game()
+print(test.grid)
+print(test.is_valid('NA'))
